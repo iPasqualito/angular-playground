5b58268a78ad284286b62ad7a9ca244640637e18
norton <pascal@mintminds.com>
2019-10-10 15:19:11 +0200

    Reacgtive form example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-playground';
+  form: FormGroup = new FormGroup({
+    myInput: new FormControl(null, [Validators.required, Validators.email], []),
+    naw: new FormGroup({
+      name: new FormControl(),
+      addresses: new FormArray([])
+    }, [])
+  });
+
+  ngOnInit() {
+
+    this.form.valueChanges.subscribe( formdata => {
+      console.log(formdata);
+    } );
+
+  }
+
+  submitForm(form){
+    console.log(form.value);
+  }
+
 }
