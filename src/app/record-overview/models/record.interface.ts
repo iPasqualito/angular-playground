4efac0ccd753f9627463ff79da89f5b4c6d17e21
Pascal Louwes <p.louwes@gmail.com>
2019-10-16 18:20:43 +0200

    Lekker knutselen! Een beetje uit de tutorial en een beetje van mezelf

---
+++ src/app/record-overview/models/record.interface.ts
@@ -0,0 +1,15 @@
+export interface Record {
+  CatalogNumber: string,
+  Artist: string,
+  Title: string,
+  Label: string,
+  Format: string,
+  Rating: string,
+  Released: number,
+  release_id: number,
+  CollectionFolder: string,
+  DateAdded: string,
+  CollectionMediaCondition: string,
+  CollectionSleeveCondition: string,
+  CollectionNotes: string
+}
